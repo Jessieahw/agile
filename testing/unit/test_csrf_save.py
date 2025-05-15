49c6200b26a6abdc98b0a5bb67ba1a97a266e208
praj107 <pranavrajput321@gmail.com>
2025-05-15 09:16:46 +0800

    Patch register forms being broken due to extra field Refs #17

--- testing/unit/test_csrf_save.py
+++ testing/unit/test_csrf_save.py
@@ -18,6 +18,6 @@ def test_save_with_csrf(client, csrf_token):
     res = client.post("/save_comparison",
                       json={"avg_shots":10,"avg_goals":1.5,
                             "avg_fouls":1,"avg_cards":1.2,
-                            "shot_accuracy":0.5,"matched_team":"X"},
+                            "shot_accuracy":0.5,"matched_team":"Tottenheim"},
                       headers={"X-CSRFToken": csrf_token})
     assert res.status_code == 200
